588f51bb8381bc2bb7e794780a54320def3b0642
mozzammelrobi <mozzammelfb@gmail.com>
2024-03-02 12:20:16 +0600

    loadPhne from api fetch and create a card and dynamicaly add api information in card

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const loadPnone = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const data = await res.json()
+    const phones = data.data
+    // console.log(phones)
+    displayPhones(phones)
+}
+
+
+
+const displayPhones = (phones) => {
+    console.log(phones)
+
+    const phonesContainer = document.getElementById('phones-container')
+
+
+    phones.forEach((phone) =>{
+        console.log(phone)
+        const phoneCard = document.createElement('div')
+  
+        phoneCard.classList = 'card card-compact bg-base-100 shadow-xl p-3'
+
+        phoneCard.innerHTML = ` 
+        <figure><img class=" lg:w-72" src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class=" flex ">
+            <button class="btn btn-primary">Show Details</button>
+          </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneCard)
+    })
+}
+
+loadPnone()
